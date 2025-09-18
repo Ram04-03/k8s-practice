c5d82d83208e1577f2b648677368033c91c5cf15
Ram0403 <ram3052610@gmail.com>
2025-09-18 15:02:13 +0000

    Initial clean commit for Kubernetes practice

---
+++ install_kind.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# For AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.29.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+#kubectl installation
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
+# and then append (or prepend) ~/.local/bin to $PATH
+
+echo "kind and kubectl installation complete"
+
